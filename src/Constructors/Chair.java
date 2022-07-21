b2d113e171311ae470a7a37cc36ea3a45d44307a
Irfan Nasim <irfan.nasim@infotechgroup.com>
2022-07-21 10:21:14 +0500

    constructors

--- src/Constructors/Chair.java
+++ src/Constructors/Chair.java
@@ -1,8 +1,52 @@
 package Constructors;
 
+// Concrete Class
+// POJO class (Plain Old Java Object
 public class Chair {
 
-    public Chair() {
+    private int legs;
+    private int back;
+    private String sitting;
 
+    public Chair() {
+    }
+
+    public Chair(int legs, int back, String sitting) {
+        this.legs = legs;
+        this.back = back;
+        this.sitting = sitting;
+    }
+
+    @Override
+    public String toString() {
+        return "Chair{" +
+                "legs=" + legs +
+                ", back=" + back +
+                ", sitting='" + sitting + '\'' +
+                '}';
+    }
+
+    public int getLegs() {
+        return legs;
+    }
+
+    public void setLegs(int legs) {
+        this.legs = legs;
+    }
+
+    public int getBack() {
+        return back;
+    }
+
+    public void setBack(int back) {
+        this.back = back;
+    }
+
+    public String getSitting() {
+        return sitting;
+    }
+
+    public void setSitting(String sitting) {
+        this.sitting = sitting;
     }
 }
--- src/Constructors/MainClass.java
+++ src/Constructors/MainClass.java
@@ -3,6 +3,14 @@ package Constructors;
 public class MainClass {
 
     public static void main(String[] args) {
-        Chair chair = new Chair();
+        Chair chair = new Chair(4, 1, "Comfortable");
+        Chair chair1 = new Chair(10, 1, "Not comfortable");
+        Chair chair2 = new Chair(5, 1, "More Comfortable");
+        Chair chair3  = new Chair();
+
+        System.out.println(chair);
+        System.out.println(chair1);
+        System.out.println(chair2);
+        System.out.println(chair3);
     }
 }
